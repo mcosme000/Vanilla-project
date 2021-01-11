99f6a6897b5f9353d6127e758975e41c91c32439
Maria Cosmen <mcosme00@gmail.com>
2021-01-11 15:42:43 +0900

    added geolocation and css changes

--- src/script.js
+++ src/script.js
@@ -11,6 +11,24 @@ function showWeather(results) {
   description.innerHTML = `${results.data.weather[0].main}`;
 }
 
+// Convert to Celsius //
+document.querySelector("#celsius").addEventListener("click", function (e) {
+  e.preventDefault();
+  var temperatureElement = document.querySelector("#tempValue");
+  var temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = (Math.round(temperature - 32) * 5) / 9;
+});
+
+// Convert to Fahrenheit //
+document.querySelector("#fah").addEventListener("click", function (e) {
+  e.preventDefault();
+  var temperatureElement = document.querySelector("#tempValue");
+  var temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round(temperature * 9) / 5 + 32;
+});
+
 /* when we click on submit button, everything starts */
 document.querySelector("#search-form").addEventListener("submit", function (e) {
   e.preventDefault();
@@ -31,7 +49,30 @@ document.querySelector("#search-form").addEventListener("submit", function (e) {
 });
 */
 
-function showLocation(location) {
-  console.log(location.data);
+function getLocation(position) {
+  console.log(position);
+
+  var lat = `${position.coords.latitude}`;
+  var lon = `${position.coords.longitude}`;
+
+  // with lat and lon variables I can get the weather API
+  // of the current geolocation.
+
+  function currentLocation(result) {
+    console.log(result);
+    var text = document.querySelector(".locationInfo");
+    var city = result.data.name;
+    var temp = Math.round(result.data.main.temp);
+    var description = result.data.weather[0].description;
+
+    text.innerHTML = `Current city: ${city}, ${temp}ºC, ${description}`;
+  }
+
+  var apiKey = "36c8bd885e1b84703cd48d295c95399d";
+  var geoapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(geoapi).then(currentLocation);
 }
-navigator.geolocation.getCurrentPosition(showLocation);
+
+document.querySelector("#geolocation").addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(getLocation);
+});
