fb5f28164b23c1575710331781169100cb2936f6
Maria Cosmen <mcosme00@gmail.com>
2021-01-11 13:15:06 +0900

    added weather api and changed some labels and styles

--- src/script.js
+++ src/script.js
@@ -1,11 +1,37 @@
-/* - - - CITY PHOTOS API - - - */
-
-function showCity(result) {
-  console.log(result.data.photos[0].image.web);
+/* - - - WEATHER API - - - */
+function showWeather(results) {
+  console.log(results.data);
+  var city = document.querySelector("#cityName");
+  var temp = document.querySelector("#tempValue");
+  var humidity = document.querySelector("#humidity");
+  var description = document.querySelector("#description");
+  city.innerHTML = `${results.data.name}`;
+  temp.innerHTML = `${results.data.main.temp}`;
+  humidity.innerHTML = `${results.data.main.humidity}%`;
+  description.innerHTML = `${results.data.weather[0].main}`;
 }
 
-var city = "";
+/* when we click on submit button, everything starts */
+document.querySelector("#search-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  var city = document.querySelector("#input").value;
+  var apiKey = "36c8bd885e1b84703cd48d295c95399d";
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+});
 
-apiUrl = `https://api.teleport.org/api/urban_areas/slug:${city}/images`;
+// - - - - -  GEOLOCATION BUTTON - - - - - //
 
-axios.get(apiUrl).then(showCity);
+/*document.querySelector(".geolocation").addEventListener("click", function (e) {
+  e.preventDefault();
+  var geoapi =
+    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+  var apiKey = "36c8bd885e1b84703cd48d295c95399d";
+  axios.get().then(showLocation);
+});
+*/
+
+function showLocation(location) {
+  console.log(location.data);
+}
+navigator.geolocation.getCurrentPosition(showLocation);
